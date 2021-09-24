fc16a107915b76dd6c4730421e2e4fdab1c89a01
Hellol77 <dhe7700@naver.com>
2021-09-24 23:51:15 +0900

    end

--- polynomial.py
+++ polynomial.py
@@ -1,11 +1,22 @@
 import time, random
 def evaluate_n2(A, x):
-	
-
+	t=0
+	ans=0
+	for i in range(len(A)):
+		t+=A[i]
+		for j in range(i):
+			t*=x
+		ans+=t
+		t=0	
+	return ans
 def evaluate_n(A, x):
+	t=A[len(A)-1]
 
+	for i in range(len(A)-2,-1,-1):
+		t=A[i]+x*t
+	return t
 
-random.seed()# random 함수 초기화
+random.seed()
 n=int(input())
 A=[]
 for i in range(n):
@@ -14,11 +25,7 @@ x=random.randint(-1000,1000)
 
 
 
-# n 입력받음
-# 리스트 A를 randint를 호출하여 n개의 랜덤한 숫자로 채움
-# evaluate_n2 호출
-# evaluate_n 호출
-# 두 함수의 수행시간 출력
+
 s=time.process_time()
 evaluate_n2(A,x)
 e=time.process_time()
