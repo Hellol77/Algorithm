8c017c827469aa64d3b9e6dd0096c2e49724331d
DongHyeonWon <dhe7700@naver.com>
2023-05-24 21:40:34 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,9 +1,3 @@
 def solution(targets):
-    answer=0
-    targets.sort(key=lambda x:x[1])
-    temp=0
-    for target in targets:
-        if temp<=target[0]:
-            answer+=1
-            temp=target[1]
+    answer = 0
     return answer
