20d53b455f4df360385e26a760f70bfdbfe92827
DongHyeonWon <dhe7700@naver.com>
2024-05-19 23:04:09 +0900

    [level 2] Title: N-Queen, Time: 9208.33 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,20 @@
+def solution(n):
+    
+    def check (ls,new):
+        for i in range(len(ls)):
+            if new == ls[i] or (len(ls)-i) == abs(ls[i]-new):
+                return False
+            
+        return True
+    
+    def dfs(ls):
+        if len(ls)==n:
+            return 1
+        cnt=0
+        for i in range(n):
+            if check(ls,i):
+                cnt+=dfs(ls+[i])
+        return cnt
+                
+    return dfs([])
+        
