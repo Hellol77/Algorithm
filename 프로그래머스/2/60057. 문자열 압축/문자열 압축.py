4a37f846032221328d079ac3555100818a11ef19
DongHyeonWon <dhe7700@naver.com>
2024-05-30 00:10:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,19 +1,3 @@
 def solution(s):
-    answer=[]
-    for i in range(1,len(s)+1):
-        a = ""
-        count = 1
-        temp = s[:i]
-        for j in range(i,len(s)+i,i):
-            if temp == s[j:j+i]:
-                count+=1
-            else:
-                if count!=1:
-                    a=a+str(count)+temp
-                else:
-                    a=a+temp
-                temp=s[j:j+i]
-                count=1
-        answer.append(len(a))
-        
-    return min(answer)
+    answer = 0
+    return answer
