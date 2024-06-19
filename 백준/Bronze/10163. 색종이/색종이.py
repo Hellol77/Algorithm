0879cc6758777f05d664adac0fe03178941e6b7f
DongHyeonWon <dhe7700@naver.com>
2024-06-19 14:21:16 +0900

    [Bronze I] Title: 색종이, Time: 276 ms, Memory: 39120 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = [[0 for i in range(1001)] for i in range(1001)]
+
+for i in range(1,n+1):
+    row,column,w,h =map(int,sys.stdin.readline().split())
+    for r in range(w):
+        for c in range(h):
+            arr[r][c]=i
+
+answer = [0]*(n+1)
+for i in arr:
+    for j in i :
+        if j:
+            answer[j]+=1
+for i in range(1,n+1):
+    print(answer[i])
