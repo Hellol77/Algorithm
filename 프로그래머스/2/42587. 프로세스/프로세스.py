39fff6fc3e3f84b0a456b3e4f330128f1378ebe7
DongHyeonWon <dhe7700@naver.com>
2024-10-04 12:14:13 +0900

    [level 2] Title: 프로세스, Time: 0.84 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(priorities, location):
+    m = max(priorities)
+    temp = []
+    for index,i in enumerate(priorities):
+        temp.append([i,index])
+    d = deque(temp)
+    result = []
+    while d:
+        if d[0][0]==m:
+            result.append(d.popleft())
+            if d:
+                max_0=max(d, key=lambda x: x[0])
+                m=max_0[0]
+        else:
+            d.append(d.popleft())
+    
+    
+    for index,i in enumerate(result):
+        if i[1]==location:
+            return index+1
+
+        
